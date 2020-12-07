06b53eaa73d711bba2590d25e687873855f8cac0
unknown <yestsering@gmail.com>
2020-12-07 15:46:15 +0100

    first day second commit

--- cour1.js
+++ cour1.js
@@ -19,3 +19,27 @@ console.log(testLength.length);
 //REPLACE is change or replace your value or index
 var food = "increasing is meh";
 console.log(food.replace("meh", "so good"));
+
+
+// UpperCase and LowerCase
+var basic = "This is Cool";
+console.log(basic);
+var basicUp = basic;
+console.log(basicUp.toUpperCase());
+var basicDown = basic;
+console.log(basicDown.toLowerCase());
+
+
+//SPLIT is split the word in to character
+var word = "banana";
+var letter = word;
+console.log(letter.split(""));
+
+
+//TEMPLATE it is similar as Concatenation
+var age = 39;
+var template = "I'm " + age + " years old";
+console.log(` Hello ${template}`);
+
+
+
