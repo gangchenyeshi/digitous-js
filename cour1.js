dfb8d542b4e8ac851228c016bd3f633d51c769dd
unknown <yestsering@gmail.com>
2020-12-07 15:23:39 +0100

    first day version one

---
+++ cour1.js
@@ -0,0 +1,21 @@
+// Display Hello World in console.log
+console.log("Hello World !");
+console.log("Welcome to javascript World");
+
+// create a variable string type
+var test = "My name is Yeshi";
+console.log(test);
+
+// Concatenation mean Join the two string together
+var name = "Gangchen";
+console.log("Nice to meet you " + name + ".");
+
+
+//String length is count the character length in a string
+var testLength = "I'm very long!";
+console.log(testLength.length);
+
+
+//REPLACE is change or replace your value or index
+var food = "increasing is meh";
+console.log(food.replace("meh", "so good"));
