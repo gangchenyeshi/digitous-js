345818ec76f16dc02effffbd03a3898934c168dc
unknown <yestsering@gmail.com>
2020-12-09 16:13:48 +0100

    day three fifth commit

--- jour03.js
+++ jour03.js
@@ -53,5 +53,13 @@ var total = 0;
 var limit = 10;
 
 for (var i = 0; i<= limit; i ++ ) {
-    console.log(total = i);
+    //console.log(total = i);
+    total +=i;
+    console.log(total)
+
 }
+
+
+//console.log(i);
+
+// Reverse 
