e7e0eaba698041d81ffd15dafc24ab82df4e76ba
unknown <yestsering@gmail.com>
2020-12-11 09:50:04 +0100

    day five first commit

--- jour03.js
+++ jour03.js
@@ -83,20 +83,7 @@ console.log("total final :" +total)
     }
  }
 
- // BONUS 2 avec WHILE LOOP
- //var total = 0;
- //var limit = 10;
- //while (total <= 10) {
-    
-   //     total += i;
-     //   console.log("total first :"  total);
-    //}
-    
- //console.log(total);
-
-
-
- 
+ // Reverse String character
 var sentence = "Hello konexio!";
 var sentenceLen=sentence.length;
 console.log(` Sentence Length : ${sentenceLen}`);
@@ -108,3 +95,62 @@ for (var i=sentenceLen-1; i>=0; i--) {
 }
 console.log(`Reverse Character is:  ${newsentence} `);
 
+
+
+
+
+//BONUS 2
+var total = 0;
+var limit = 10;
+var i = 0;
+while ( i< 10) {
+    i ++ ;
+    console.log("i : " + i);
+    total +=i;
+    //1) total = 0 + 1(i) = 1
+    //2) total = 1 + 2(i) = 3
+    //3) total = 3 + 3(i) = 6
+    //4) total = 6 + 4(i) = 10
+    //;....
+    //10) total = 45 + 10(i) = 55
+
+
+}
+console.log("total final :" +total)
+
+
+
+//BONUS 3
+//Créez un tableau avec le nom de chacun de vos camarades de promo
+//Affichez aléatoirement le nom de la personne tiré au sort
+
+var promo4 = ["Ahmed", "Mohammed", "Houra", "Mohamad Bailo", "Saurav", "Kitty", "Elisabeth", "Nicolas", "Yeshi Tsering", "Mahmoudou", "Allan", "Ewa", "Mosindo", "Hassan", "Sébastien", "Damir", "Hadama", "Thi", "Mohamed", "Atif"];
+ var max=promo4.length - 1; //
+ var min=0;
+ var random= Math.floor(Math.random() * (max-min+1)+min); 
+console.log(random);
+ while (random===18){
+     //if random 18 === 18 then re execute the random
+    random = Math.floor(Math.random() * (max - min + 1) +min);
+    //random = 18 
+    // 18 ===18 yes then start random again
+    //random =12
+    // 12===18 no exit the loop
+ }
+ console.log(random);
+ console.log(promo4[random]);
+
+
+
+ // Bonus 2
+ var total = 0;
+ var limit = 10;
+ var i = 0;
+ while ( i < 10 ) {
+     console.log(`value of i :  ${i}`);
+     i++;
+     total += i; //total= total + i
+ }
+ console.log(`value of total :  ${total}`);
+
+ 
--- jour04.js
+++ jour04.js
@@ -1,11 +1,88 @@
 var number = 7;
 var mulNumber="";
- for(i=1; i<=10; i++) {
+ for(i=0; i<=10; i++) {
      mulNumber = i * number
      // number * 1 = 7
      //number * 2 = 14
      //.....        70
-     console.log(mulNumber); 
+     console.log(`${number} * ${i} = ${mulNumber}`); 
      
  }
+ var min=1;
+ var max=6;
+
+//console.log(dice);
+
+ for (var i=0; i<10; i++) {
+    //console.log(Math.floor(Math.random() * (max-min+1)));
+    var dice= Math.floor(Math.random() * (max-min+1));
+    console.log(dice)
+ }
  
+// sum total at end for 10 times
+ var min=1;
+ var max=6;
+
+//console.log(dice);
+var totalDice=0;
+ for (var i=0; i<10; i++) {
+    //console.log(Math.floor(Math.random() * (max-min+1)));
+    var dice= Math.floor(Math.random() * (max-min+1));
+    console.log(`each dice : ${dice} `);
+    totalDice = totalDice + dice;
+ }
+ console.log(`sum total of the dice : ${totalDice} ` );
+
+
+
+ console.log(`value of i ${sumInteger}`);
+
+ //afficher la multiplication de tout les entier allant de 10 à 15
+ var mulNumber=1;
+ for(var i=10; i<=15; i++) {
+     console.log(`value of i ${i}`);
+     mulNumber =mulNumber * i;
+     
+ }
+ console.log(mulNumber);
+
+ // afficher la somme des entiers pairs allant de 0 à 100 (resultat: 2550)
+ var pairNumber = 0;
+ for (i=0; i<=100;  i= i + 2) {
+    console.log(`value of i ${i}`);
+    pairNumber = pairNumber + i;
+ }
+ console.log(` sum of pairs value ${pairNumber}`);
+
+ //: afficher tout les multiples de 7 entre 0 et 1000 (edited) 
+var multipleNumber = 7;
+var result=0;
+ for(i=0; i<=1000; i++) {
+    //console.log(`value of i is : ${i}`);
+    result = multipleNumber * i;
+    console.log(`${multipleNumber} * ${i} =${result}`);
+ }
+
+ //: afficher tout les multiples de 7 entre 0 et 1000 (edited) 
+ 
+ var result=0;
+ for(var i=0; i<=1000; i++) {
+    //console.log(`value of i is : ${i}`);
+    if(i % 7 === 0) {
+        console.log(i);
+    }
+}
+ 
+ 
+
+
+
+//AFFICHER LA SOMME DE TOUT LES ENTIER ALLANT DE 25 à 75
+var sumInteger=0; 
+
+ var i = 25; 
+ while(i<=75) {
+    sumInteger +=i;
+    i++;
+ }
+ console.log(sumInteger);  
