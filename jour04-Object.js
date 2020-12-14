b4f21a8c19d2b0874aa95106b7f5e128c9ebf891
unknown <yestsering@gmail.com>
2020-12-14 14:23:08 +0100

    day four Object 03

--- jour04-Object.js
+++ jour04-Object.js
@@ -35,3 +35,19 @@ if(cat2.isCute===true) {
 var cats=[cat, cat2];
 console.log(cat.age);
 console.log(cat2.isCute);
+
+
+//03 EVEN
+//- Créez une fonction `checkIfEven` qui reçoit un paramètre `num`
+//- Dans la fonction, à l'aide d'une condition, affichez "even" si `num` est pair, "odd" si il est impair
+//- Appelez votre fonction plusieurs fois avec des chiffres différents comme argument pour vérifier les résultats
+
+
+function checkIfEven(num){
+    if(num % 2 ===0){
+        console.log(`Num is even : ${num}`);
+    }else {
+        console.log(`Num is odd : ${num}`);
+    }
+}
+checkIfEven(3);
