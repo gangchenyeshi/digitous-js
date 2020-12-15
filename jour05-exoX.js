29da9c068f950457a0730e69ace9d10541d2af20
unknown <yestsering@gmail.com>
2020-12-15 16:09:46 +0100

    day five 3 commit

--- jour05-exoX.js
+++ jour05-exoX.js
@@ -1,5 +1,5 @@
 // 01 - Calculator
-
+/*
 function calculate(number1, operator, number2) {
     var result =0;
     var num1 = parseInt(number1);
@@ -12,11 +12,15 @@ function calculate(number1, operator, number2) {
 //console.log("Si vous ne donnez pas trois arguments, le programme doit afficher error dans la console")
 
 console.log(process.argv);
-console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
-
+//console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+if(process.argv.length===5) {
+    console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+} else {
+    console.log("Si vous ne donnez pas trois arguments, le programme doit afficher error dans la console")
+}
 //calculate(5, 4)
 
-
+*/
 
 
 
@@ -27,17 +31,16 @@ console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
 //- Vous devrez donner l'argument dans le terminal en appelant le programme
 
 //⇒ Si vous ne donnez pas d'argument, le programme doit afficher "error" dans la console//
+
+
 function multiply(num) {
     //var num=2;    
     var result = 1;
     for(var i=1; i<=10; i++) {
         result = num * i;
-        console.log(result);
-    }
-    
-    
+        console.log(`${num} x ${i} = ${result} `);
+        //return result;
+    }    
 }
-
-//console.log("if you don't give an argument, the program must display erro in the console")
 console.log(process.argv);
 multiply(process.argv[2]);
