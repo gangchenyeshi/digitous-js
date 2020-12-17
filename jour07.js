7ae153263063d4b637ff42e55544c009c6df8005
unknown <yestsering@gmail.com>
2020-12-17 15:28:58 +0100

    day six 3 commit

--- jour07.js
+++ jour07.js
@@ -8,9 +8,29 @@ function sortLetter(newString) {
     var alphabet=newString.split('').sort().join('');
     console.log(alphabet);
 
-    
-
 }
 sortLetter("konexio");
 
+//02 XOXO
+// - Create a `countEach` function which receives a string containing x and o as parameter
+// - The function returns true if there are as many x as there are o, otherwise it returns false
+// - Check that the "xxxoooox" argument triggers a true, and that "xox" triggers a false
 
+function countEach(xo) {
+    var newString=xo.split('');
+    console.log(newString);
+    /*
+    var varX="x";
+    var countx=newString.indexOf(varX);
+    console.log(countx);
+  
+    var varO="o";
+    var counto=newString.indexOf(varO);
+    console.log(counto);
+    */
+    var countX=newString.filter(i=>i==="x").length;
+    console.log(`number of x : ${countX}`);
+    var countO=newString.filter(i=>i==="o").length;
+    console.log(`Number of x : $(countO)`)
+}
+countEach("xxxoooox");
